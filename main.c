a67a5524b42b2d4ac64a95057a5d20795447a59b
angelo sacchiero <angelo.sacchiero@gmail.com>
2025-10-13 12:55:58 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Canzone {
+    char *titoloCanzone;
+    char *nomeArtista;
+    int minutiDurata;
+    int secondiDurata;
+};
+
+int main(void) {
+    printf("Benvenuto su spotify!\n");
+
+
+    return 0;
+}
