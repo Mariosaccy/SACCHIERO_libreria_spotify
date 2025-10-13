bb67286b909b49e19aa5c298bcd6ead42ba6380f
angelo sacchiero <angelo.sacchiero@gmail.com>
2025-10-13 13:29:32 +0200

    Added function for song insertion

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct Canzone {
     char *titoloCanzone;
@@ -7,8 +9,66 @@ struct Canzone {
     int secondiDurata;
 };
 
+void insermento(struct Canzone **canzoni, int index) {
+
+    char *titoloCanzoneIn;
+    char *nomeArtistaIn;
+    int minutiDurataIn;
+    int secondiDurataIn;
+
+    printf("Insert titoloCanzoneIn: ");
+    fgets(titoloCanzoneIn, 40, stdin);
+    printf("Insert nomeArtistaIn: ");
+    fgets(nomeArtistaIn, 40, stdin);
+    printf("Insert minutiDurataIn: ");
+    scanf("%d", &minutiDurataIn);
+    printf("Insert secondiDurataIn: ");
+    scanf("%d", &secondiDurataIn);
+
+    canzoni[index]->titoloCanzone = titoloCanzoneIn;
+    canzoni[index]->nomeArtista = nomeArtistaIn;
+    canzoni[index]->minutiDurata = minutiDurataIn;
+    canzoni[index]->secondiDurata = secondiDurataIn;
+}
+
 int main(void) {
-    printf("Benvenuto su spotify!\n");
+
+    int scelta;
+
+    struct Canzone **canzoni = (struct Canzone **) malloc(sizeof(struct Canzone*));
+    int index = 0;
+
+    do {
+        printf("Benvenuto su spotify!\n");
+        printf("1 -> Aggiungi canzone\n");
+        printf("2 -> Visualizza libreria\n");
+        printf("3 -> Esci\n");
+
+        printf("la tua scelta: ");
+        scanf("%d", &scelta);
+
+        switch (scelta) {
+            case 1:
+                insermento(canzoni, index);
+                index++;
+                canzoni = (struct Canzone **) realloc(canzoni, (index+1) * sizeof(struct Canzone*));
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            default:
+                printf("errore, input errato\n");
+                break;
+        }
+
+    }while(scelta != 3);
+
 
 
     return 0;
